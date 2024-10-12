6fe9850be160edce7de87dfc08915c3dce245f54
A B M Asadullah <abmasadullah@gmail.com>
2024-10-12 15:29:11 +0600

    Header

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import googleMapReact from 'google-map-react'
+import { Paper, Typography, useMediaQuery } from '@material-ui/core'
+
+
+import useStyles from './styles'
 
 const Map = () => {
+  // classes = useStyles();
   return (
     <div>Map</div>
   )
